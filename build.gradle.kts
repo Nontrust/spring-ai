2c021ebb11f78ae1a682da750c2c59ee8b273ca8
nontrust <deed1515@naver.com>
2026-02-03 00:24:52 +0900

    :tada: init spring ai

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.ai.spring"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springAiVersion"] = "1.0.0"
+
+dependencies {
+    /*
+    // OpenAI 사용 시
+    implementation("org.springframework.ai:spring-ai-starter-model-openai")
+    // Anthropic 사용 시
+    implementation("org.springframework.ai:spring-ai-starter-model-anthropic")
+    // Google Gemini 사용 시
+    implementation("org.springframework.ai:spring-ai-starter-model-google-genai")
+    */
+    /** web */
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    /** kotlin */
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    /** test */
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
